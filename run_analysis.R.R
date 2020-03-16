ae9cc12307aa210b9f6815623bf47ff97fd7d5d1
Jonathan Ho <jonathan.ho2468@gmail.com>
2020-03-16 15:13:12 -0700

    Ensures that meanFreq columns are not parsed
    replaces code with activity description
    
    organizes data so there is one value per observation

--- run_analysis.R.R
+++ run_analysis.R.R
@@ -9,12 +9,13 @@ test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test_x <- read.table("UCI HAR Dataset/test/x_test.txt",sep = "",header = FALSE,nrows = 2947)
 test_y <- read.table("UCI HAR Dataset/test/y_test.txt")
 
-features <- read.table("UCI HAR Dataset/features.txt")
+features <- read.table("UCI HAR Dataset/features.txt") 
 
 
 "Creating a test data frame with the different column names"
 
-colnames(test_x) <- features[[2]]
+"Appropriately labels the data set with descriptive variable names."
+colnames(test_x) <- features[[2]]  
 test_x$identifier <- test_subject[[1]]
 test_x$activity <- test_y[[1]]
 
@@ -25,12 +26,13 @@ train_subject <- read.table("UCI HAR Dataset/train/subject_train.txt")
 train_x <- read.table("UCI HAR Dataset/train/x_train.txt",sep = "",header = FALSE)
 train_y <- read.table("UCI HAR Dataset/train/y_train.txt")
 
-features <- read.table("UCI HAR Dataset/features.txt")
+features <- read.table("UCI HAR Dataset/features.txt") 
 
 
 "Creating a train data frame with the different column names"
 
-colnames(train_x) <- features[[2]]
+"Appropriately labels the data set with descriptive variable names."
+colnames(train_x) <- features[[2]] 
 train_x$identifier <- train_subject[[1]]
 train_x$activity <- train_y[[1]]
 
@@ -42,7 +44,40 @@ train_x$activity <- train_y[[1]]
 merged_data <- rbind(test_x,train_x)
 
 
-print(dim(merged_data))
+"Extracts only the measurements on the mean and standard deviation for each measurement"
+
+names <- names(merged_data)
+
+"looks for the columns with mean or std in the label"
+
+selectedmeanFreq <- grep("meanFreq()",names)
+selectedmean <- grep("mean()",names)
+selectedstd <- grep("std()",names)
+
+"indexes of columns with mean or std and removes indexes of meanFreq"
+selected <- append(selectedmean,selectedstd) 
+nomeanFreq <- which(!(selected%in%selectedmeanFreq))
+selected <- selected[nomeanFreq]
+
+myselecteddata <- merged_data[,(ncol(merged_data)-1):ncol(merged_data)]
+myselecteddata <- cbind(myselecteddata,merged_data[selected])
+
+"Uses descriptive activity names to name the activities in the data set"
+
+replaced <- function(descriptor){ "function that replaces code with activity description"
+  descriptions <- c("WALKING","WALKING UPSTAIRS","WALKING DOWNSTARIS","SITTING","STANDING","LAYING")
+  return(descriptions[descriptor])
+}
+
+myselecteddata$activity <- sapply(myselecteddata$activity,replaced)
+
+sorteddata <- arrange(myselecteddata,identifier,activity)
+
+"Organizes the data so there is one mean for each observation. First groups the data by identifier and activity and then takes the mean of each column by group"
+
+tidydata <- sorteddata %>% group_by(identifier,activity) %>%summarize_each(funs = mean) %>%
+  print
+
 
 
 

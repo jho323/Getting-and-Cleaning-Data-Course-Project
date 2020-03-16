ac2a4cb51d8e1af963252f0be25d3caff8fb5166
Jonathan Ho <jonathan.ho2468@gmail.com>
2020-03-15 23:58:01 -0700

    only reading x_test data while adding labels from features

--- test_analysis.R
+++ test_analysis.R
@@ -1,24 +1,22 @@
-test_subject <- read.delim("UCI HAR Dataset/test/subject_test.txt")
-test_x <- read.csv("UCI HAR Dataset/test/x_test.txt",sep = "",header = FALSE,nrows = 2946)
-test_y <- read.delim("UCI HAR Dataset/test/y_test.txt")
-acc_x_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt",sep = "",header = FALSE,nrows = 2946)
-acc_y_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt",sep = "",header = FALSE,nrows = 2946)
-acc_z_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt",sep = "",header = FALSE,nrows = 2946)
-gyro_x_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt",sep = "",header = FALSE,nrows = 2946)
-gyro_y_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt",sep = "",header = FALSE,nrows = 2946)
-gyro_z_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt",sep = "",header = FALSE,nrows = 2946)
-total_acc_x_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt",sep = "",header = FALSE,nrows = 2946)
-total_acc_y_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt",sep = "",header = FALSE,nrows = 2946)
-total_acc_z_test<- read.csv("UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt",sep = "",header = FALSE,nrows = 2946)
-
-
 library(tidyr)
 library(dplyr)
 
-mytest_df <- data.frame(identifier= test_subject[[1]], activity=test_y[[1]], test_set = test_x[[1]],acc_x_test = acc_x_test[[1]],
-                        acc_y_test=acc_y_test[[1]],acc_z_test=acc_z_test[[1]],gyro_x_test=gyro_x_test[[1]],gyro_y_test=gyro_y_test[[1]],
-                        gyro_z_test=gyro_z_test[[1]],total_acc_x_test=total_acc_x_test[[1]],total_acc_y_test=total_acc_y_test[[1]],
-                        total_acc_z_test=total_acc_z_test[[1]],stringsAsFactors = FALSE)
-mytest_df <- tbl_df(mytest_df)
 
-print(mytest_df)
+test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test_x <- read.table("UCI HAR Dataset/test/x_test.txt",sep = "",header = FALSE,nrows = 2947)
+test_y <- read.table("UCI HAR Dataset/test/y_test.txt")
+
+features <- read.table("UCI HAR Dataset/features.txt")
+
+
+colnames(test_x) <- features[[2]]
+test_x$identifier <- test_subject[[1]]
+test_x$activity <- test_y[[1]]
+
+
+
+print(names(test_x))
+
+
+
+
